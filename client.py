bdae808a1b506d821bb86a5a72ec100444f6df24
Alexander Bykov <alardus@alardus.org>
2016-12-04 02:33:03 +0300

    verbose

--- client.py
+++ client.py
@@ -3,14 +3,19 @@ from requests import get
 import time
 
 def push():
-	client_token = ''
-	subdomain = ''
-	ip = get('https://api.ipify.org').text
+	try:
+		print ('Ok, preparing...')
+		client_token = ''
+		subdomain = ''
+		ip = get('https://api.ipify.org').text
+		print ('Updating:\n' + '- host: ' + '"' + subdomain+ '"\n'+ '- addr: ' + ip)
 
-	urllib.request.urlopen('http://104.131.201.115:5000/updatehost?subdomain=' + subdomain + '&ip=' + ip +
-'&client_token=' +
-client_token)
-time.sleep(1800)
+		urllib.request.urlopen('http://104.131.201.115:5000/updatehost?subdomain=' + subdomain + '&ip=' + ip +
+	'&client_token=' + client_token)
+		print ('Successful\nSleep for 30 min')
+		time.sleep(1800)
+	except Exception as e:
+		print ('Oh shi...')
 
 while True:
 	push()
